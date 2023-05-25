ceba7f8716bcab3f0970cb25e8ec30dcbba7731e
mza-codes <muzofficial90@gmail.com>
2023-05-26 00:23:03 +0530

    feat: responsive header, update minor components

--- src/components/sub-components/UserAvatar.tsx
+++ src/components/sub-components/UserAvatar.tsx
@@ -1,6 +1,6 @@
 export default function UserAvatar() {
     return (
-        <div className="p-[1.5px] rounded-2xl border-2 border-gray.200">
+        <div className="p-[1px] rounded-2xl border-[1px] border-gray.200">
             <img
                 className="object-cover rounded-2xl w-12"
                 src="/assets/avatar.png"
