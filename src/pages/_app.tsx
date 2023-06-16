cbb1be1266432bd34881e93053de37f291a48e2d
mza-codes <muzofficial90@gmail.com>
2023-06-16 19:51:41 +0530

    fix: build try #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+// @ts-ignore
 import MainLayout from "@/layouts/MainLayout";
 
 type Props = AppProps & {
